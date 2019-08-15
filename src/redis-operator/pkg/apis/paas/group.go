5ac75d04ebc159dc01a8729978335b1906ac6e6d
luckyfengyong <luckyfengyong@gmail.com>
2019-08-15 15:22:22 +0000

    $GOPATH/src directory

---
+++ src/redis-operator/pkg/apis/paas/group.go
@@ -0,0 +1,6 @@
+// Package paas contains paas API versions.
+//
+// This file ensures Go source parsers acknowledge the paas package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package paas
